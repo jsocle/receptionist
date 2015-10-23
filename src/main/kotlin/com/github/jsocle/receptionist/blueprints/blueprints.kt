603ab05158ffb3f0edee521e07db48541c14b672
vamf12 <vamf12@gmail.com>
2015-10-23 16:01:29 +0900

    메인화면 레이이웃 추가

--- src/main/kotlin/com/github/jsocle/receptionist/blueprints/blueprints.kt
+++ src/main/kotlin/com/github/jsocle/receptionist/blueprints/blueprints.kt
@@ -1,7 +1,8 @@
 package com.github.jsocle.receptionist.blueprints
 
 import com.github.jsocle.Blueprint
+import com.github.jsocle.receptionist.layout
 
 object mainApp : Blueprint() {
-    val index = route("/") { -> }
+    val index = route("/") { -> layout() }
 }
