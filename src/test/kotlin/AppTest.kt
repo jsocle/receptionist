3dd645eb0e8982865305ea06a8a17819ddb95eca
vamf12 <vamf12@gmail.com>
2015-10-23 19:06:57 +0900

    예약 수정 하기 추가

--- src/test/kotlin/AppTest.kt
+++ src/test/kotlin/AppTest.kt
@@ -10,6 +10,8 @@ import com.github.jsocle.requests.Request
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
+import java.text.SimpleDateFormat
+import java.util.*
 
 class AppTest {
     @Before
@@ -77,16 +79,29 @@ class AppTest {
         app.db.session {
             Assert.assertEquals(0, it.createCriteria(Reservation::class.java).list().size)
         }
+
         var client = login()
         // test creation
         client.get(
                 reservationApp.new_.url("startAt" to "2016-01-01 09:00", "endAt" to "2016-01-01 10:00"),
                 method = Request.Method.POST
         )
-
+        var reservationId: Int = 0
         app.db.session {
             val reservation = it.createCriteria(Reservation::class.java).uniqueResult() as Reservation
-            Assert.assertEquals(1, reservation)
+            Assert.assertEquals(newDate(2016, 0, 1, 9, 0, 0).format(), reservation.startAt.format())
+            Assert.assertEquals(newDate(2016, 0, 1, 10, 0, 0).format(), reservation.endAt.format())
+            reservationId = reservation.id!!
+        }
+
+        client.get(
+                reservationApp.edit.url(reservationId, "startAt" to "2016-02-01 09:00", "endAt" to "2016-02-01 10:00"),
+                method = Request.Method.POST
+        )
+        app.db.session {
+            val reservation = it.createCriteria(Reservation::class.java).uniqueResult() as Reservation
+            Assert.assertEquals(newDate(2016, 1, 1, 9, 0, 0).format(), reservation.startAt.format())
+            Assert.assertEquals(newDate(2016, 1, 1, 10, 0, 0).format(), reservation.endAt.format())
         }
     }
 
@@ -94,5 +109,14 @@ class AppTest {
         val client = app.client
         client.get(loginApp.login.url("id" to "john", "password" to "john"), method = Request.Method.POST)
         return client
+    }
+
+    private fun newDate(year: Int, month: Int, date: Int, hourOfDay: Int, minute: Int, second: Int): Date {
+        return Calendar.getInstance().let { it.set(year, month, date, hourOfDay, minute, second); it.time }
+    }
+
+    private val sdf = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+    fun Date.format(): String {
+        return sdf.format(this)
     }
 }
